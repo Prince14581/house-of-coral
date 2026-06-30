95ba8fcc1b745c75cc622dd31e7bec96743489f2
house-of-coral <isimgodwinprince@gmail.com>
2026-06-30 15:10:17 +0100

    Create controllers/Controller.js

---
+++ controllers/Controller.js
@@ -0,0 +1,12 @@
+const User = require('../models/User');
+
+exports.register = async (req, res) => {
+    try {
+        const { username, email, password } = req.body;
+        const newUser = new User({ username, email, password });
+        await newUser.save();
+        res.status(201).json({ message: "User registered successfully!" });
+    } catch (err) {
+        res.status(400).json({ error: "Registration failed: " + err.message });
+    }
+};
