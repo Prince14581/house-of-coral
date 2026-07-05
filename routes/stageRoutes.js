0691b17635c792cea973c5d2f21e517f9676990a
house-of-coral- <houseofcoral258@gmail.com>
2026-07-05 10:41:11 +0100

    Create stageRoutes.js

---
+++ routes/stageRoutes.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const stageController = require('../controllers/stageController');
+const apiGateway = require('../middleware/apiGateway');
+
+router.use(apiGateway('Stage'));
+
+router.post('/create', stageController.createStream);
+// Future: router.get('/monitor/:streamId', stageController.getStreamHealth);
+
+module.exports = router;
